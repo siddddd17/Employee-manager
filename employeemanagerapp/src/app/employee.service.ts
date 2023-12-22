6f875e2e898a821d898a49fdbded2d6e79e0cc8d
siddharth ajith <siddharthajith1717@gmail.com>
2023-12-21 21:05:52 -0800

    Implement CORS configuration

--- employeemanagerapp/src/app/employee.service.ts
+++ employeemanagerapp/src/app/employee.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
+import { environment } from 'src/environments/environment.development';
 
 
 @Injectable({
@@ -9,7 +10,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private apiServerUrl='';
+  private apiServerUrl="http://localhost:8080"; 
 
   constructor(private http: HttpClient) {}
   
